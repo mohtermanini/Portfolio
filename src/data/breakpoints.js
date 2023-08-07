35102f360ed57f1a8b2fc708af1ec5d7345ff0f7
Mohamad Termanini <mohtermanini.job@gmail.com>
2023-08-07 21:44:02 +0400

    Add Theme + Breakpoints

---
+++ src/data/breakpoints.js
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  mobile: "550px",
+  tablet: "768px",
+  laptop: "1024px",
+  desktop: "1440px",
+  widescreen: "1920px",
+};
---
+++ src/data/theme.js
@@ -0,0 +1,61 @@
+export const theme = {
+  typography: {
+    display: {
+      md: {
+        bold: "700 48px/62px Roboto",
+      },
+      sm: {
+        bold: "700 26px/40px Roboto",
+      },
+    },
+    headline: {
+      lg: {
+        semi: "600 28px/36px Roboto",
+        regular: "400 28px/36px Roboto",
+      },
+      md: {
+        bold: "700 24px/32px Roboto",
+        semi: "600 24px/32px Roboto",
+        regular: "400 24px/32px Roboto",
+      },
+      sm: {
+        semi: "600 20px/28px Roboto",
+        regular: "400 20px/28px Roboto",
+      },
+      xs: {
+        semi: "600 17px/24px Roboto",
+        regular: "400 17px/24px Roboto",
+      },
+    },
+    body: {
+      lg: {
+        semi: "600 24px/32px Roboto",
+        medium: "500 24px/32px Roboto",
+        regular: "400 24px/32px Roboto",
+      },
+      md: {
+        semi: "600 18px/24px Roboto",
+        medium: "500 17px/24px Roboto",
+        regular: "400 17px/24px Roboto",
+      },
+      sm: {
+        semi: "600 15px/20px Roboto",
+        medium: "500 15px/20px Roboto",
+        regular: "400 15px/20px Roboto",
+      },
+      xs: {
+        semi: "600 13px/16px Roboto",
+        medium: "500 13px/16px Roboto",
+        regular: "400 13px/16px Roboto",
+      },
+    },
+  },
+  spacing: {
+    gaps: {
+      leftIconText: "8px",
+    },
+  },
+  palette: {
+    white: "#fff",
+  },
+};

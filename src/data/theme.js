a8330ac1cd88650690939fca89fd44e68dc0a59d
Mohamad Termanini <mohtermanini.job@gmail.com>
2023-08-08 21:47:41 +0400

    Modify Values

--- src/data/theme.js
+++ src/data/theme.js
@@ -1,55 +1,100 @@
 export const theme = {
   typography: {
     display: {
-      md: {
+      xl: {
         bold: "700 48px/62px Roboto",
       },
+      lg: {
+        bold: "700 35px/49px Roboto",
+      },
+      md: {
+        bold: "700 30px/44px Roboto",
+      },
       sm: {
-        bold: "700 34px/48px Roboto",
+        bold: "700 26px/40px Roboto",
       },
       xs: {
-        bold: "700 26px/40px Roboto",
+        bold: "700 22px/36px Roboto",
       },
     },
     headline: {
       lg: {
-        semi: "600 28px/36px Roboto",
+        bold: "700 28px/36px Roboto",
         regular: "400 28px/36px Roboto",
       },
       md: {
         bold: "700 24px/32px Roboto",
-        semi: "600 24px/32px Roboto",
         regular: "400 24px/32px Roboto",
       },
       sm: {
-        semi: "600 20px/28px Roboto",
+        bold: "700 20px/28px Roboto",
         regular: "400 20px/28px Roboto",
       },
       xs: {
-        semi: "600 17px/24px Roboto",
+        bold: "700 17px/24px Roboto",
         regular: "400 17px/24px Roboto",
       },
     },
-    body: {
+    title: {
+      xl: {
+        bold: "700 28px/32px Roboto",
+        regular: "400 28px/32px Roboto",
+      },
       lg: {
-        semi: "600 24px/32px Roboto",
-        medium: "500 24px/32px Roboto",
-        regular: "400 24px/32px Roboto",
+        bold: "700 24px/28px Roboto",
+        regular: "400 24px/28px Roboto",
       },
       md: {
-        semi: "600 18px/24px Roboto",
-        medium: "500 17px/24px Roboto",
-        regular: "400 17px/24px Roboto",
+        bold: "700 20px/24px Roboto",
+        regular: "400 20px/28px Roboto",
       },
       sm: {
-        semi: "600 15px/20px Roboto",
-        medium: "500 15px/20px Roboto",
-        regular: "400 15px/20px Roboto",
+        bold: "700 18px/22px Roboto",
+        regular: "400 18px/28px Roboto",
       },
       xs: {
-        semi: "600 13px/16px Roboto",
-        medium: "500 13px/16px Roboto",
-        regular: "400 13px/16px Roboto",
+        bold: "700 16px/20px Roboto",
+        regular: "400 16px/28px Roboto",
+      },
+    },
+    body: {
+      xxl: {
+        regular: "400 24px/32px Roboto",
+      },
+      xl: {
+        regular: "400 22px/30px Roboto",
+      },
+      lg: {
+        regular: "400 20px/28px Roboto",
+      },
+      md: {
+        regular: "400 18px/26px Roboto",
+      },
+      sm: {
+        regular: "400 16px/24px Roboto",
+      },
+      xs: {
+        regular: "400 14px/22px Roboto",
+      },
+    },
+    label: {
+      xxl: {
+        regular: "400 18px/20px Roboto",
+      },
+      xl: {
+        regular: "400 16px/18px Roboto",
+      },
+      lg: {
+        regular: "400 14px/16px Roboto",
+      },
+      md: {
+        regular: "400 12px/14px Roboto",
+      },
+      sm: {
+        regular: "400 10px/12px Roboto",
+      },
+      xs: {
+        regular: "400 8px/10px Roboto",
       },
     },
   },

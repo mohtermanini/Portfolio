643c26581983cb472c80e6c9e43d69d400d0b8a7
Mohamad Termanini <mohtermanini.job@gmail.com>
2023-08-10 06:14:15 +0400

    Modify Config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let basePath = "/";
+let assetPrefix = "";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  basePath = `/${repo}`;
+  assetPrefix = `/${repo}/`;
+}
+
 const nextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
+    loader: "imgix",
+    path: "https://mohamadtermanini-121374497.imgix.net",
     remotePatterns: [
       {
         protocol: "http",

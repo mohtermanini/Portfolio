569a542b3fab76e307030e851136005a0b8dc898
Mohamad Termanini <mohtermanini.job@gmail.com>
2023-09-02 19:56:15 +0400

    Add Font Support Using next/font/google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Roboto } from "next/font/google";
 import Providers from "@/components/Providers/Providers";
 import { StyledComponentsRegistry } from "@/styles/StyledComponentsRegistry";
 import "../assets/sass/loader.scss";
@@ -7,13 +8,21 @@ export const metadata = {
   description: "Portfolio of Mohamad Termanini",
 };
 
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${roboto.variable}`}>
       <body>
         <div className="initial-loader">
           <div className="dots-wrapper">
-            <span class="dots">
+            <span className="dots">
               <span></span>
               <span></span>
               <span></span>
--- src/data/theme.js
+++ src/data/theme.js
@@ -2,105 +2,105 @@ export const theme = {
   typography: {
     display: {
       xl: {
-        bold: "700 48px/62px Roboto",
+        bold: "700 48px/62px var(--font-roboto)",
       },
       lg: {
-        bold: "700 35px/49px Roboto",
+        bold: "700 35px/49px var(--font-roboto)",
       },
       md: {
-        bold: "700 30px/44px Roboto",
+        bold: "700 30px/44px var(--font-roboto)",
       },
       sm: {
-        bold: "700 26px/40px Roboto",
+        bold: "700 26px/40px var(--font-roboto)",
       },
       xs: {
-        bold: "700 22px/36px Roboto",
+        bold: "700 22px/36px var(--font-roboto)",
       },
     },
     headline: {
       lg: {
-        bold: "700 28px/36px Roboto",
-        regular: "400 28px/36px Roboto",
+        bold: "700 28px/36px var(--font-roboto)",
+        regular: "400 28px/36px var(--font-roboto)",
       },
       md: {
-        bold: "700 24px/32px Roboto",
-        regular: "400 24px/32px Roboto",
+        bold: "700 24px/32px var(--font-roboto)",
+        regular: "400 24px/32px var(--font-roboto)",
       },
       sm: {
-        bold: "700 20px/28px Roboto",
-        regular: "400 20px/28px Roboto",
+        bold: "700 20px/28px var(--font-roboto)",
+        regular: "400 20px/28px var(--font-roboto)",
       },
       xs: {
-        bold: "700 17px/24px Roboto",
-        regular: "400 17px/24px Roboto",
+        bold: "700 17px/24px var(--font-roboto)",
+        regular: "400 17px/24px var(--font-roboto)",
       },
     },
     title: {
       xl: {
-        bold: "700 28px/32px Roboto",
-        regular: "400 28px/32px Roboto",
+        bold: "700 28px/32px var(--font-roboto)",
+        regular: "400 28px/32px var(--font-roboto)",
       },
       lg: {
-        bold: "700 24px/28px Roboto",
-        regular: "400 24px/28px Roboto",
+        bold: "700 24px/28px var(--font-roboto)",
+        regular: "400 24px/28px var(--font-roboto)",
       },
       md: {
-        bold: "700 20px/24px Roboto",
-        regular: "400 20px/28px Roboto",
+        bold: "700 20px/24px var(--font-roboto)",
+        regular: "400 20px/28px var(--font-roboto)",
       },
       sm: {
-        bold: "700 18px/22px Roboto",
-        regular: "400 18px/28px Roboto",
+        bold: "700 18px/22px var(--font-roboto)",
+        regular: "400 18px/28px var(--font-roboto)",
       },
       xs: {
-        bold: "700 16px/20px Roboto",
-        regular: "400 16px/28px Roboto",
+        bold: "700 16px/20px var(--font-roboto)",
+        regular: "400 16px/28px var(--font-roboto)",
       },
     },
     body: {
       xxl: {
-        regular: "400 24px/36px Roboto",
+        regular: "400 24px/36px var(--font-roboto)",
       },
       xl: {
-        regular: "400 22px/34px Roboto",
+        regular: "400 22px/34px var(--font-roboto)",
       },
       lg: {
-        regular: "400 20px/32px Roboto",
+        regular: "400 20px/32px var(--font-roboto)",
       },
       md: {
-        regular: "400 18px/30px Roboto",
+        regular: "400 18px/30px var(--font-roboto)",
       },
       sm: {
-        regular: "400 16px/28px Roboto",
+        regular: "400 16px/28px var(--font-roboto)",
       },
       xs: {
-        regular: "400 14px/26px Roboto",
+        regular: "400 14px/26px var(--font-roboto)",
       },
     },
     label: {
       xxl: {
-        bold: "700 18px/20px Roboto",
-        regular: "400 18px/20px Roboto",
+        bold: "700 18px/20px var(--font-roboto)",
+        regular: "400 18px/20px var(--font-roboto)",
       },
       xl: {
-        bold: "700 16px/18px Roboto",
-        regular: "400 16px/18px Roboto",
+        bold: "700 16px/18px var(--font-roboto)",
+        regular: "400 16px/18px var(--font-roboto)",
       },
       lg: {
-        bold: "700 14px/16px Roboto",
-        regular: "400 14px/16px Roboto",
+        bold: "700 14px/16px var(--font-roboto)",
+        regular: "400 14px/16px var(--font-roboto)",
       },
       md: {
-        bold: "700 12px/14px Roboto",
-        regular: "400 12px/14px Roboto",
+        bold: "700 12px/14px var(--font-roboto)",
+        regular: "400 12px/14px var(--font-roboto)",
       },
       sm: {
-        bold: "700 10px/12px Roboto",
-        regular: "400 10px/12px Roboto",
+        bold: "700 10px/12px var(--font-roboto)",
+        regular: "400 10px/12px var(--font-roboto)",
       },
       xs: {
-        bold: "700 8px/10px Roboto",
-        regular: "400 8px/10px Roboto",
+        bold: "700 8px/10px var(--font-roboto)",
+        regular: "400 8px/10px var(--font-roboto)",
       },
     },
   },
